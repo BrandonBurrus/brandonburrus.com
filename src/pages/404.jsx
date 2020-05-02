dba5d9b879566aa86df8c781052aadbcc0ed75e3
Brandon Burrus <brandon@burrus.io>
2020-05-01 19:38:08 -0700

    chore: add eslint config

---
+++ src/pages/404.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const NotFoundPage = () => (
+  <main>
+    <h1>Page not found!</h1>
+  </main>
+)
+
+export default NotFoundPage
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const IndexPage = () => (
+  <main>
+    <h1>Brandon Burrus</h1>
+    <h2>
+      I'm a <span>Software Engineer</span>
+    </h2>
+  </main>
+)
+
+export default IndexPage
