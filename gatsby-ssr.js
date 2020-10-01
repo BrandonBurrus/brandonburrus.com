ff2d955d1bcfcceed437e94497f463be9fcf26d5
Brandon Burrus <brandon@burrus.io>
2020-10-01 12:58:36 -0700

    fix: resolve static css build issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,3 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import App from './src/app/App';
 
-// You can delete this file if you're not using it
+export const wrapRootElement = App;
