bbb84d5961a8b55c40c9d2ba1e7e81c0bdc92984
Brandon Burrus <brandon@burrus.io>
2021-01-03 13:43:44 -0700

    feat: add redirect route for resume

---
+++ src/pages/resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Redirect page to resume
+ */
+function resume() {
+  return <meta httpEquiv='refresh' content='0; URL=/BrandonBurrusResume.pdf' />;
+}
+
+export default resume;
